074029420dae43792e5634774f56a218b06ef160
David KALIPE <kalipedavid@gmail.com>
2025-01-25 10:16:50 +0000

    finishing routes management

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,9 +1,9 @@
-import 'package:advanced_flutter/presentation/color_manager.dart';
-import 'package:advanced_flutter/presentation/styles_manager.dart';
-import 'package:advanced_flutter/presentation/values_manager.dart';
+import 'package:advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter/presentation/resources/styles_manager.dart';
+import 'package:advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
 
-import 'font_manager.dart';
+import '../resources/font_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
